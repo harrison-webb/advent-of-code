b4fccad611962269f0b52f92056669114c01a4e7
Harrison Webb <harrisonwebb8@gmail.com>
2022-12-08 23:16:32 -0700

    day 7 part 1 ✅ LETS GOOOO

--- aoc2022/day07.py
+++ aoc2022/day07.py
@@ -1,5 +1,5 @@
 def part_one():
-    filesystem = Tree("root", None)
+    filesystem = Tree("/", None)
     current = filesystem
     with open("input07.txt") as file:
         for line in file.read().split("\n"):
@@ -18,8 +18,69 @@ def part_one():
                     current.subdirectories[tokens[1]] = Tree(tokens[1], current)
                 else:
                     # last possibility is a file is listed, so add {filename: filesize} to current.files
-                    current.files.append({tokens[1]: tokens[0]})
-    print(filesystem.subdirectories["/"].subdirectories)
+                    current.files.append((tokens[1], int(tokens[0])))
+    calculate_node_sizes(filesystem)
+    return sum(find_nodes_smaller_than(100000, filesystem))
+
+
+def part_one_better():
+    directories = {}
+    current_directories = []
+    with open("input07.txt") as file:
+        for line in file.read().split("\n"):
+            tokens = line.split()
+            if line == "$ cd ..":
+                # remove most recent directory from current_directories
+                current_directories.pop()
+            elif line.startswith("$ cd"):
+                # append to current_directories
+                if tokens[2] not in directories:
+                    directories[tokens[2]] = 0
+                current_directories.append(tokens[2])
+            elif line.startswith("dir"):
+                # might not need this
+                True
+            elif line[0].isnumeric():
+                # add size of file to each directory in current_directories
+                for dir in current_directories:
+                    directories[dir] += int(tokens[0])
+    sum = 0
+    for key, value in directories.items():
+        if value <= 100000:
+            sum += value
+    return sum
+
+
+def parse_tree(tree):
+    size = 0
+
+    for name, directory in tree.subdirectories.items():
+        size += parse_tree(directory)
+    for file in tree.files:
+        size += file[1]
+    if size <= 100000:
+        return size
+    else:
+        return 0
+
+
+def find_nodes_smaller_than(size, tree):
+    node_list = []
+    if tree.size <= size:
+        node_list.append(tree.size)
+    for name, directory in tree.subdirectories.items():
+        node_list.extend(find_nodes_smaller_than(size, directory))
+    return node_list
+
+
+def calculate_node_sizes(tree):
+    size = 0
+    for file in tree.files:
+        size += file[1]
+    for name, directory in tree.subdirectories.items():
+        size += calculate_node_sizes(directory)
+    tree.size = size
+    return size
 
 
 class Tree:
@@ -28,6 +89,7 @@ class Tree:
         self.subdirectories = {}
         self.parent = parent
         self.name = name
+        self.size = 0
 
 
-part_one()
+print(part_one())
