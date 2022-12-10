5d254114972f6bbca3a30f54aa002fa4a8aaa0f0
Harrison Webb <harrisonwebb8@gmail.com>
2022-12-10 15:05:32 -0700

    day 8 ✅

--- aoc2022/day08.py
+++ aoc2022/day08.py
@@ -18,6 +18,67 @@ def part_one():
     return visible_trees
 
 
+def part_two():
+    max = 0
+    tree_grid = None
+    with open("input08.txt") as file:
+        tree_grid = file.read().split()
+    for i in range(1, len(tree_grid) - 1):
+        for j in range(1, len(tree_grid[0]) - 1):
+            score = calculate_senic_score(i, j, tree_grid)
+            if score > max:
+                max = score
+    return max
+
+
+def calculate_senic_score(i, j, grid):
+    # count number of trees left, right, above, and below tree at (i,j), terminating when we see a
+    # tree with height greater or equal to tree at (i,j)
+
+    return (
+        visible_trees_above(i, j, grid)
+        * visible_trees_below(i, j, grid)
+        * visible_trees_left(i, j, grid)
+        * visible_trees_right(i, j, grid)
+    )
+
+
+def visible_trees_above(i, j, grid):
+    visible_trees = 0
+    for k in range(i - 1, -1, -1):
+        visible_trees += 1
+        if grid[k][j] >= grid[i][j]:
+            break
+    return visible_trees
+
+
+def visible_trees_below(i, j, grid):
+    visible_trees = 0
+    for k in range(i + 1, len(grid)):
+        visible_trees += 1
+        if grid[k][j] >= grid[i][j]:
+            break
+    return visible_trees
+
+
+def visible_trees_left(i, j, grid):
+    visible_trees = 0
+    for k in range(j - 1, -1, -1):
+        visible_trees += 1
+        if grid[i][k] >= grid[i][j]:
+            break
+    return visible_trees
+
+
+def visible_trees_right(i, j, grid):
+    visible_trees = 0
+    for k in range(j + 1, len(grid[0])):
+        visible_trees += 1
+        if grid[i][k] >= grid[i][j]:
+            break
+    return visible_trees
+
+
 def is_visible_top(i, j, grid):
     height = grid[i][j]
     for k in range(i):
@@ -50,4 +111,4 @@ def is_visible_right(i, j, grid):
     return True
 
 
-print(part_one())
+print(part_two())
