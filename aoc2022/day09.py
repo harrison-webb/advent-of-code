3cadbcd6b9fa39f0a0d4698d40417843808a9337
Harrison Webb <harrisonwebb8@gmail.com>
2022-12-11 15:03:58 -0700

    day 9 ✅ getting rid of the silliness of tracking previous position of knots and just calculating where a knot needs to move did the trick

--- aoc2022/day09.py
+++ aoc2022/day09.py
@@ -28,4 +28,54 @@ def part_one():
     return len(tail_position_list)
 
 
-print(part_one())
+def part_two():
+    f = open("input09.txt")
+    input = f.read().split("\n")
+    f.close()
+    segments = []
+    for i in range(10):
+        segments.append([0, 0])
+    tail_position = segments[-1]
+    tail_position_list = set()
+    tail_position_list.add(tuple(tail_position))
+
+    for command in input:
+        tokens = command.split()
+        direction = tokens[0]
+        steps = int(tokens[1])
+        for i in range(steps):
+            if direction == "U":
+                segments[0] = [segments[0][0], segments[0][1] + 1]
+            elif direction == "D":
+                segments[0] = [segments[0][0], segments[0][1] - 1]
+            elif direction == "L":
+                segments[0] = [segments[0][0] - 1, segments[0][1]]
+            elif direction == "R":
+                segments[0] = [segments[0][0] + 1, segments[0][1]]
+            for j in range(len(segments)):
+                if j == 0:
+                    continue
+                if (
+                    (segments[j - 1][0] - segments[j][0]) ** 2
+                    + (segments[j - 1][1] - segments[j][1]) ** 2
+                ) ** 0.5 > 1.5:
+                    dx = dy = 0
+                    unit = []
+                    x = segments[j - 1][0] - segments[j][0]
+                    y = segments[j - 1][1] - segments[j][1]
+                    if x == 0:
+                        unit = [0, int(y / abs(y))]
+                    elif y == 0:
+                        unit = [int(x / abs(x)), 0]
+                    else:
+                        unit = [int(x / abs(x)), int(y / abs(y))]
+                    segments[j] = [
+                        segments[j][0] + unit[0],
+                        segments[j][1] + unit[1],
+                    ]
+                    tail_position_list.add(tuple(segments[-1]))
+    print(tail_position_list)
+    return len(tail_position_list)
+
+
+print(part_two())
